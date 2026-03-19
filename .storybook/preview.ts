0ff4b1132af2a3e92f38a6c1ef220b9924a45168
kning <kning@example.com>
2026-03-19 17:31:59 +0800

    配置(storybook): 更新侧边栏导航排序并启用MDX支持

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,7 +8,13 @@ const preview: Preview = {
   parameters: {
     options: {
       storySort: {
-        order: ['通用', '数据录入', '*'],
+        order: [
+          '通用', 
+          ['颜色 Colors', '阴影 Shadows', '栅格 Grid', '按钮 Button'],
+          '导航', 
+          '数据录入', 
+          '*'
+        ],
       },
     },
     docs: {
